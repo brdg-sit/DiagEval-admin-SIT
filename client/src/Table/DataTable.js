e97d74e7598a64b2de125f98a201727cc013007c
ideook <ideook@gmail.com>
2022-05-18 13:41:48 +0900

    사용량 가져오기

--- client/src/Table/DataTable.js
+++ client/src/Table/DataTable.js
@@ -1,37 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import "./DataTable.css";
+
+const baseuri = "https://sitapi.brdg.kr/api/sit/";
+
+const loadGridCols = [
+  { field: "col1", headerName: "월", width: 150 },
+  { field: "col2", headerName: "가스사용량", width: 100, type: "number" },
+  { field: "col3", headerName: "전기사용량", width: 100, type: "number" },
+  { field: "col4", headerName: "id", width: 50, hideable: false, hide: true },
+];
 
 const DataTable = () => {
-  const [tableData, setTableData] = useState([]);
-  const [tableHeader, setTableHeader] = useState([]);
-
-  const [rows, setRows] = useState(tableData);
-  const [deletedRows, setDeletedRows] = useState([]);
+  const [dataUserEnter, setDataUserEnter] = useState([]);
+  const [headerUserEnter, setHeaderUserEnter] = useState([]);
+  const [dataLoad, setDataLoad] = useState([]);
+  const [headerLoad, setHeaderLoad] = useState([]);
 
   const fetchTableHeader = async () => {
-    var url = new URL("https://sitapi.brdg.kr/api/sit/tableinfo");
+    var url = new URL(baseuri + "tableinfo");
     var params = { nmTable: "tbl_user_enter" };
     url.search = new URLSearchParams(params).toString();
 
     await fetch(url)
       .then((data) => data.json())
-      .then((data) => setTableHeader(data));
+      .then((data) => setHeaderUserEnter(data));
   };
 
   const fetchTableData = async () => {
-    var url = new URL("https://sitapi.brdg.kr/api/sit/mldata");
+    var url = new URL(baseuri + "mldata");
 
     await fetch(url)
       .then((data) => data.json())
-      .then((data) => setTableData(data));
+      .then((data) => setDataUserEnter(data));
   };
 
   const fetchTableUserData = async () => {
-    var url = new URL("https://sitapi.brdg.kr/api/sit/userdata");
+    var url = new URL(baseuri + "userdata");
 
     await fetch(url)
       .then((data) => data.json())
-      .then((data) => setTableData(data));
+      .then((data) => setDataUserEnter(data));
   };
 
   useEffect(() => {
@@ -39,26 +48,90 @@ const DataTable = () => {
     fetchTableUserData();
   }, []);
 
+  const handleGetLoad = async (e) => {
+    const postData = {
+      id: e.row.id,
+    };
+
+    try {
+      const res = await fetch(baseuri + "load", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": "token-value",
+        },
+        body: JSON.stringify(postData),
+      });
+      if (!res.ok) {
+        const message = `An error has occured: ${res.status} - ${res.statusText}`;
+        throw new Error(message);
+      }
+      const data = await res.json();
+      const result = {
+        status: res.status + "-" + res.statusText,
+        headers: {
+          "Content-Type": res.headers.get("Content-Type"),
+          "Content-Length": res.headers.get("Content-Length"),
+        },
+        data: data,
+      };
+      let loads = [];
+      for (let i = 0; i < data.length; i++) {
+        const d = data[i];
+
+        loads.push({
+          id: d.id,
+          col1: d.mnth,
+          col2: d.load_gas,
+          col3: d.load_elec,
+          col4: d.id_etr,
+        });
+      }
+      setDataLoad(loads.map((row) => ({ ...row })));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div style={{ height: 900, width: "100%" }}>
-      <DataGrid
-        rows={tableData}
-        columns={tableHeader}
-        pageSize={100}
-        checkboxSelection
-        disableSelectionOnClick
-        // onSelectionModelChange={({ selectionModel }) => {
-        //   const rowIds = selectionModel.map((rowId) =>
-        //    parseInt(String(rowId), 10)
-        //   );
-        //   const rowsToDelete = tableData.filter((row) =>
-        //    rowIds.includes(row.id)
-        //   );
-        //   setDeletedRows(rowsToDelete);
-        //   console.log(deletedRows);
-        // }}
-      />
-    </div>
+    <>
+      <div className="area_top">
+        <div className="buttonArea">
+          <button className="button">저장</button>
+        </div>
+      </div>
+      <div className="area_bottom">
+        <div className="area_grid1">
+          <div className="area_button_grid">
+            <span className="title_grid">제목1</span>
+            <button className="button_inside">저장</button>
+          </div>
+          <div className="grid">
+            <DataGrid
+              rows={dataUserEnter}
+              columns={headerUserEnter}
+              pageSize={100}
+              checkboxSelection
+              onRowClick={handleGetLoad}
+            />
+          </div>
+        </div>
+        <div className="area_grid2">
+          <div className="area_button_grid">
+            <span>제목1</span>
+            <button className="button_inside">저장</button>
+          </div>
+          <div className="grid">
+            <DataGrid
+              rows={dataLoad}
+              columns={loadGridCols}
+              pageSize={100}
+              checkboxSelection
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
